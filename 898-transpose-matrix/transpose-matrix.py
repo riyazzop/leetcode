7306c9a099b9114d1e69b3604d6503e6ada5d8eb
Shaik Riyaz  Basha <riyaz.shaikit@gmail.com>
2025-12-14 10:20:12 +0530

    Time: 3 ms (28.94%) | Memory: 13.1 MB (33.26%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.py
+++ 898-transpose-matrix/transpose-matrix.py
@@ -6,15 +6,15 @@ class Solution(object):
         """
         n=len(matrix)
         m=len(matrix[0])
-        # if m==n:
-        #     for i in range(n):
-        #         for j in range(i+1,n):
-        #             matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
-        #     return matrix
-        # else:
-        revmat=[[0 for i in range(n)] for j in range(m)]
-        for i in range(m):
-            for j in range(n):
-                revmat[i][j]=matrix[j][i]
-        return revmat
+        if m==n:
+            for i in range(n):
+                for j in range(i+1,n):
+                    matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+            return matrix
+        else:
+            revmat=[[0 for i in range(n)] for j in range(m)]
+            for i in range(m):
+                for j in range(n):
+                    revmat[i][j]=matrix[j][i]
+            return revmat
         
