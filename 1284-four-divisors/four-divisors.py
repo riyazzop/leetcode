541df2615e032753c8458854060a6ec487d00e7e
Shaik Riyaz  Basha <riyaz.shaikit@gmail.com>
2026-01-04 22:54:36 +0530

    Time: 321 ms (68.00%) | Memory: 13.1 MB (62.67%) - LeetSync

--- 1284-four-divisors/four-divisors.py
+++ 1284-four-divisors/four-divisors.py
@@ -16,19 +16,19 @@ class Solution(object):
                         divisers.append(num//i)
             if len(divisers)==4:
                 total+=sum(divisers)
-        return total
+        return total 
 
-        # total=0
-        # for num in nums:
-        #     divisers=[]
-        #     for i in range(1,int(sqrt(num))+1):
-        #         if len(divisers)>4:
-        #             break
-        #         if num%i==0:
-        #             divisers.append(i)
-        #             otherdiviser=num//i
-        #             if i!=otherdiviser:
-        #                 divisers.append(otherdiviser)
-        #         if len(divisers)==4:
-        #             total+=sum(divisers)
-        # return total
+        total=0
+        for num in nums:
+            divisers=[]
+            for i in range(1,int(sqrt(num))+1):
+                if len(divisers)>4:
+                    break
+                if num%i==0:
+                    divisers.append(i)
+                    otherdiviser=num//i
+                    if i!=otherdiviser:
+                        divisers.append(otherdiviser)
+            if len(divisers)==4:
+                total+=sum(divisers)
+        return total
