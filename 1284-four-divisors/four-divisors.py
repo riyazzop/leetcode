3d6429a6ecfe0120090e7688bf8b6e6d3b38fe1b
Shaik Riyaz  Basha <riyaz.shaikit@gmail.com>
2026-01-04 22:54:54 +0530

    Time: 331 ms (66.67%) | Memory: 13 MB (82.67%) - LeetSync

--- 1284-four-divisors/four-divisors.py
+++ 1284-four-divisors/four-divisors.py
@@ -4,19 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        total=0
-        for num in nums:
-            divisers=[]
-            for i in range(1,int(sqrt(num+1))+1):
-                if len(divisers)>4:
-                    break
-                if num%i==0:
-                    divisers.append(i)
-                    if i!=num//i:
-                        divisers.append(num//i)
-            if len(divisers)==4:
-                total+=sum(divisers)
-        return total 
+        # total=0
+        # for num in nums:
+        #     divisers=[]
+        #     for i in range(1,int(sqrt(num+1))+1):
+        #         if len(divisers)>4:
+        #             break
+        #         if num%i==0:
+        #             divisers.append(i)
+        #             if i!=num//i:
+        #                 divisers.append(num//i)
+        #     if len(divisers)==4:
+        #         total+=sum(divisers)
+        # return total 
 
         total=0
         for num in nums:
