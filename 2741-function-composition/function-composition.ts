60409c87820b05c4bdb708b986755a25b1b0fca5
Shaik Riyaz  Basha <riyaz.shaikit@gmail.com>
2026-02-01 12:36:15 +0530

    Time: 58 ms (43.74%) | Memory: 56.8 MB (51.04%) - LeetSync

---
+++ 2741-function-composition/function-composition.ts
@@ -0,0 +1,17 @@
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+    
+    return function(x) {
+        if(functions.length===0)return x
+        for(let i=functions.length-1;i>-1;i--){
+            x=functions[i](x)
+        }
+        return x
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
