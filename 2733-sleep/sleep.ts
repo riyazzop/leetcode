1c5fd842449a093ccca0fc94289d424753d06732
Shaik Riyaz  Basha <riyaz.shaikit@gmail.com>
2026-02-02 09:48:57 +0530

    Time: 40 ms (83.78%) | Memory: 55.8 MB (6.47%) - LeetSync

---
+++ 2733-sleep/sleep.ts
@@ -0,0 +1,13 @@
+async function sleep(millis: number): Promise<void> {
+    await new Promise((res,rej)=>{
+        setTimeout(()=>{
+            res(millis)
+        },millis)
+    })
+}
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
