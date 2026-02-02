f8313fa010a8c7e362d8435fc1e0c02a962b298f
Shaik Riyaz  Basha <riyaz.shaikit@gmail.com>
2026-02-02 09:38:43 +0530

    Time: 220 ms (98.87%) | Memory: 96.2 MB (37.58%) - LeetSync

---
+++ 2731-memoize/memoize.ts
@@ -0,0 +1,26 @@
+type Fn = (...params: number[]) => number
+
+function memoize(fn: Fn): Fn {
+    const cache=new Map()
+    return function(...args) {
+        const key=args.join(',')
+        if(cache.has(key)){
+            return cache.get(key)
+        }
+        const res=fn(...args)
+        cache.set(key,res)
+        return res
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
