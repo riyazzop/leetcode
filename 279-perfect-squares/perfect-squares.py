a139b2597cb864be3a98549b4ea87195719a6cf7
Shaik Riyaz  Basha <riyaz.shaikit@gmail.com>
2026-03-04 22:51:24 +0530

    Time: 3045 ms (61.37%) | Memory: 12.7 MB (66.08%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp=[n]*(n+1)
+        dp[0]=0
+        for target in range(1,n+1):
+            for s in range(1,int(sqrt(target))+1):
+                square=s*s
+                dp[target]=min(dp[target],1+dp[target-square])
+        return dp[n]
