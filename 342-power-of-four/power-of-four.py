117288dc3341fa0a3fb39353ea61fb43817c1a25
Shaik Riyaz  Basha <riyaz.shaikit@gmail.com>
2026-03-06 10:35:24 +0530

    Time: 5 ms (2.84%) | Memory: 12.3 MB (98.36%) - LeetSync

--- 342-power-of-four/power-of-four.py
+++ 342-power-of-four/power-of-four.py
@@ -4,7 +4,4 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        if (n-1)&n==0:
-            if (n-1)%3==0:
-                return True
-        return False
+        return True if(n&(n-1)==0 and (n-1)%3==0) else False
